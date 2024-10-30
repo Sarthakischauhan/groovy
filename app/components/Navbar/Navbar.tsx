75f8f531021901be7943dbdb1170da99babac476
Sarthakischauhan <sarthakchauhann@gmail.com>
2024-10-29 17:59:17 -0700

    Sign in page design done

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,15 +1,27 @@
 import Link from "next/link";
+import { auth, signOut } from "@/utils/auth";
+import { Button } from "../ui/Button";
+import { ExitIcon } from "@radix-ui/react-icons";
 
 interface NavbarProps{};
 
-const  Navbar: React.FC<NavbarProps> = () => {
+const Navbar: React.FC<NavbarProps> = async () => {
+  const session = await auth();
   return (
     <nav className="sm:w-9/10 md:w-3/5 mx-auto flex items-center justify-between p-4 bg-white text-black">
       <div className="text-2xl font-bold font-antonio">Groovy</div>
+      {session && 
       <div className="space-x-4">
         <Link href="/" className="hover:text-gray-600">Dashboard</Link>
         <Link href="/questions" className="hover:text-gray-600">Questions</Link>
+        <Button className="signout">
+          <ExitIcon />
+        </Button>
       </div>
+      }
+      {!session && 
+        <Link href="/login" className="hover:text-gray-600">Login</Link>
+      }
     </nav>
   );
 }
--- app/components/Signin.tsx
+++ app/components/Signin.tsx
@@ -1,14 +1,40 @@
 import { signIn } from "@/utils/auth"
- 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/Card";
+import { Button } from "./ui/Button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
 export default function SignIn() {
   return (
     <form
       action={async () => {
         "use server"
-        await signIn("github")
+        await signIn("github", { redirectTo: "/"})
       }}
     >
-      <button type="submit">Signin with GitHub</button>
+      <Card className="w-[400px] m-auto">
+        <CardHeader>
+          <CardTitle>Sign In</CardTitle>
+          <CardDescription>Login using anyone of these methods</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <div>
+              <Button className="w-full" type="submit">
+                <GitHubLogoIcon className="mx-2" /> 
+                <span>Signin with GitHub</span> 
+              </Button>
+            </div>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </form>
   )
 } 
