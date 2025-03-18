1e9acb999b3e9139f60704000e0d904b36b29051
Sarthakischauhan <sarthakchauhann@gmail.com>
2025-03-17 22:02:29 -0700

    minor changes on ui and codebase.

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default async function Home() {
     <>
       <SessionProvider session={session}>
         {(userData && session?.user?.isLoggedIn) ? (
-          <div className="sm:w-9/10 md:w-3/5 mx-auto">
+          <div className="sm:w-9/10 md:w-4/5 lg:w-3/5 mx-auto">
             <ManagementHeader users={[userData]} />
             <div className="w-full">
               <h1 className="w-full p-4 text-l md:text-3xl font-semibold font-inter">Recent Expense</h1>
